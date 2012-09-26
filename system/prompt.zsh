0d5875229c28e6e29ae1648f18d4a0c3d90c28f4
Michael Heap <m@michaelheap.com>
2012-09-26 19:01:54 +0100

    Change the colour of the branch name depending on the git index

--- system/prompt.zsh
+++ system/prompt.zsh
@@ -1,5 +1,7 @@
 autoload -U colors && colors
 
+ICON="▶  "
+
 function prompt_char {
     git branch >/dev/null 2>/dev/null && echo '±' && return
     hg root >/dev/null 2>/dev/null && echo '☿' && return
@@ -9,19 +11,22 @@ function prompt_char {
 function git_branch {
 	BRANCH="$(git symbolic-ref HEAD 2>/dev/null | cut -d'/' -f3)"
 	if ! test -z $BRANCH; then
-		echo "$BRANCH"
+		COL="%{$fg[green]%}" # Everything's fine
+		[[ $(git log origin/master..HEAD 2> /dev/null ) != "" ]] && COL="%{$fg[blue]%}" # We have changes to push
+		[[ $(git status --porcelain 2> /dev/null) != "" ]] && COL="%{$fg[red]%}" # We have uncommited changes
+		echo "$COL$BRANCH"
 	fi
 }
 
 function get_git_dirty { 
-   [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]] && echo "%{$fg[red]%}*%{$reset_color%}"
+   [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]] && echo "%{$fg[blue]%}"
 }
 
 
 function precmd() {
-	PROMPT="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ $(get_git_dirty)%{$reset_color%}% 
+	PROMPT="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}% 
 $(prompt_char) "
-	RPROMPT="%{$fg[red]%}% $(git_branch)%{$reset_color%}%"
+	RPROMPT="$(git_branch)%{$reset_color%}%"
 
 	title $(pwd | sed -e "s,^$HOME,~,")
 }
