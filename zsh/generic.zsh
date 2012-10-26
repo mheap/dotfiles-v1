021b4ae33b8af7b255496fd71ba8882c7e2cad9c
Michael Heap <m@michaelheap.com>
2012-10-26 11:21:21 +0100

    ZSH Syntax Highlighting working

--- zsh/generic.zsh
+++ zsh/generic.zsh
@@ -3,4 +3,5 @@ bindkey "^[[3~" delete-char
 
 # ZSH Syntax Highlighting?
 source $ZSH/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
