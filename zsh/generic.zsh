8ad5dc530752c14a0aa31b16b83f2901e86d912e
Michael Heap <m@michaelheap.com>
2012-10-30 15:47:49 +0000

    Add more smart defaults to .vimrc. Start Vundle removal process

--- zsh/generic.zsh
+++ zsh/generic.zsh
@@ -1,5 +1,7 @@
 # Make fn-backspace work on OSX
 bindkey "^[[3~" delete-char
+bindkey -v
+bindkey -M viins 'jj' vi-cmd-mode
 
 # ZSH Syntax Highlighting?
 source $ZSH/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
