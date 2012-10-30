9042cb7d2bafb86cbf7612ff7f8759e789703582
Michael Heap <m@michaelheap.com>
2012-10-30 17:39:29 +0000

    Add comments to bundles

--- vim/.vim.symlink/update_bundles.rb
+++ vim/.vim.symlink/update_bundles.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/env ruby
 
 # shamelessly stolen from https://github.com/tsaleh/dotfiles/blob/master/vim/update_bundles
-git_bundles = %w{
-    git://github.com/altercation/vim-colors-solarized.git
-    git://github.com/Lokaltog/vim-easymotion.git
-    git://github.com/myusuf3/numbers.vim.git
-    git://github.com/scrooloose/nerdtree.git
-    git://github.com/vim-scripts/LustyJuggler.git
-    git://github.com/kien/ctrlp.vim.git
-}
+git_bundles = [
+
+    ## Colourschemes
+    "git://github.com/altercation/vim-colors-solarized.git", 
+    
+    ## Plugins
+    "git://github.com/Lokaltog/vim-easymotion.git", # Quick navigation inside a buffer
+    "git://github.com/myusuf3/numbers.vim.git", # Relative line numbers in normal mode
+    "git://github.com/scrooloose/nerdtree.git", # Tree file viewer
+    "git://github.com/vim-scripts/LustyJuggler.git", # Buffer manager
+    "git://github.com/kien/ctrlp.vim.git", # Fuzzy matching file opener
+
+    ## Syntax files
+    "git://github.com/tpope/vim-markdown.git", # Markdown syntax
+    "git://github.com/pangloss/vim-javascript.git" # Better Javascript indentation
+]
 
 require 'fileutils'
 require 'open-uri'
