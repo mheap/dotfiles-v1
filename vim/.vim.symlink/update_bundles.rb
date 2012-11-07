a9af12bb5891ddb6a559207d240a81e7681833e0
Michael Heap <m@michaelheap.com>
2012-11-07 11:41:54 +0000

    Add css-color, delimitmate and closetag

--- vim/.vim.symlink/update_bundles.rb
+++ vim/.vim.symlink/update_bundles.rb
@@ -13,11 +13,14 @@ git_bundles = [
     "git://github.com/vim-scripts/LustyJuggler.git", # Buffer manager
     "git://github.com/kien/ctrlp.vim.git", # Fuzzy matching file opener
     "git://github.com/vim-scripts/taglist.vim.git", # Taglist (needs ctags)
-    "git://github.com/vim-scripts/AutoTag.git", # Renegerage ctags when file is saved
+    "git://github.com/vim-scripts/AutoTag.git", # Renegerate ctags when file is saved
+    "git://github.com/Raimondi/delimitMate.git", # Smart quote/parethesis closing
+    "git://github.com/docunext/closetag.vim.git", # Auto close HTML tags
 
     ## Syntax files
     "git://github.com/tpope/vim-markdown.git", # Markdown syntax
-    "git://github.com/pangloss/vim-javascript.git" # Better Javascript indentation
+    "git://github.com/pangloss/vim-javascript.git", # Better Javascript indentation
+    "git://github.com/skammer/vim-css-color.git" # Show CSS colours in vim
 ]
 
 require 'fileutils'
