069255668447496e477deca91188b43e6e7aa7f7
Michael Heap <m@michaelheap.com>
2012-12-05 19:10:38 +0000

    Add i3 backup to gitignore. Add prettyjson to bin

---
+++ machine-specific/mike/aliases.zsh
@@ -0,0 +1,10 @@
+# SSHFS
+alias sshfsl='sshfs lorenzo0:/var/www/services /mnt/vm_lorenzo0; cd /mnt/vm_lorenzo0'
+
+# Moving around
+alias cpd='cd /mnt/vm_lorenzo0/pushdelivery'
+alias cps='cd /mnt/vm_lorenzo0/pushscheduler'
+
+# Vim
+alias epd='e /mnt/vm_lorenzo0/pushdelivery'
+alias eps='e /mnt/vm_lorenzo0/pushscheduler'
