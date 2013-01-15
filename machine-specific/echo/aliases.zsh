39cf509e0874bcd46fd5d7c3e258ef31e9e4616f
Michael Heap <m@michaelheap.com>
2013-01-15 10:06:04 +0000

    Bringing things up to date

---
+++ machine-specific/echo/aliases.zsh
@@ -0,0 +1,2 @@
+alias cpstop='sudo launchctl stop com.crashplan.engine;sudo launchctl unload /Library/LaunchDaemons/com.crashplan.engine.plist'
+alias cpstart='sudo launchctl load /Library/LaunchDaemons/com.crashplan.engine.plist;sudo launchctl start com.crashplan.engine'
