61e4011430e323633762bd9c782ac299e0d980dc
Michael Heap <m@michaelheap.com>
2013-08-01 19:13:23 +0100

    Update composer. Update vim bundles. Start using tabs in vim. Remove rbenv from zsh + remove cursor highlighting that was causing it to disappear

--- zsh/generic.zsh
+++ zsh/generic.zsh
@@ -8,7 +8,7 @@ command -v xkbset >/dev/null 2>&1 && { xkbset -sl; xkbset -a; }
 
 # ZSH Syntax Highlighting?
 source $ZSH/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
 
 # History searching
