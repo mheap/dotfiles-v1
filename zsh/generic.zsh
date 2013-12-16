6c28e063a8d17e7df6c0d654c196b992b01ffc55
Michael Heap <m@michaelheap.com>
2013-12-16 11:10:38 +0000

    zsh: More vim bindings, autocomplete for known hosts

--- zsh/generic.zsh
+++ zsh/generic.zsh
@@ -2,6 +2,23 @@
 bindkey "^[[3~" delete-char
 bindkey -v
 bindkey -M viins 'jj' vi-cmd-mode
+# Use vim cli mode
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+# backspace and ^h working even after
+# returning from command mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+
+# ctrl-w removed word backwards
+bindkey '^w' backward-kill-word
+
+# ctrl-r starts searching history backward
+bindkey '^r' history-incremental-search-backward
+
+# Key timeout for vim mode
+export KEYTIMEOUT=20
 
 # Slowkeys no more!
 command -v xkbset >/dev/null 2>&1 && { xkbset -sl; xkbset -a; }
@@ -13,3 +30,8 @@ ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
 
 # History searching
 source $ZSH/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+if [ -e $HOME/.ssh/known_hosts ] ; then
+    hosts=(${${${(f)"$(<$HOME/.ssh/known_hosts)"}%%\ *}%%,*})
+    zstyle ':completion:*:hosts' hosts $hosts
+fi
